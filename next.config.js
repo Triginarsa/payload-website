f9023b21fcd24fa27b4e6f803979e14fa25d079a
triginarsa <adi@cakeplabs.com>
2024-02-07 06:37:37 +0800

    change csp

--- next.config.js
+++ next.config.js
@@ -1,50 +1,86 @@
 /** @type {import('next').NextConfig} */
 const ContentSecurityPolicy = require('./csp')
-const redirects = require('./redirects')
+const isDev = process.env.NODE_ENV !== 'production'
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.USE_ANALYZER,
+})
+
+const resolveApiDomain = () => {
+  const apiUrl = process.env.NEXT_PUBLIC_PAYLOAD_SERVER_URL
+  try {
+    const url = new URL(apiUrl)
+    return url.hostname
+  } catch {
+    return 'localhost'
+  }
+}
 
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL]
-      .filter(Boolean)
-      .map(url => url.replace(/https?:\/\//, '')),
+  reactStrictMode: false,
+  publicRuntimeConfig: {
+    SERVER_URL: process.env.NEXT_PUBLIC_PAYLOAD_SERVER_URL,
   },
-  redirects,
+  images: {
+    minimumCacheTTL: 6000,
+    remotePatterns: [
+      // {
+      //     protocol: isDev ? 'http' : 'https',
+      //     hostname:  isDev ? 'localhost' : resolveApiDomain(),
+      //     port: isDev ? '8000' : '',
+      //     pathname: '/media/**'
+      // }
+      {
+        protocol: 'https',
+        hostname: resolveApiDomain(),
+        port: '',
+        pathname: '/media/**',
+      },
+    ],
+  },
   async headers() {
     const headers = []
-
-    // Prevent search engines from indexing the site if it is not live
-    // This is useful for staging environments before they are ready to go live
-    // To allow robots to crawl the site, use the `NEXT_PUBLIC_IS_LIVE` env variable
-    // You may want to also use this variable to conditionally render any tracking scripts
-    if (!process.env.NEXT_PUBLIC_IS_LIVE) {
-      headers.push({
-        headers: [
-          {
-            key: 'X-Robots-Tag',
-            value: 'noindex',
-          },
-        ],
-        source: '/:path*',
-      })
-    }
+    headers.push({
+      headers: [
+        {
+          key: 'Strict-Transport-Security',
+          value: 'max-age=63072000; includeSubDomains; preload',
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'origin-when-cross-origin',
+        },
+      ],
+      source: '/:path*',
+    })
+    // if (!process.env.NEXT_PUBLIC_IS_LIVE) {
+    //     headers.push({
+    //         headers: [
+    //             {
+    //                 key: 'X-Robots-Tag',
+    //                 value: 'noindex',
+    //             },
+    //         ],
+    //         source: '/:path*',
+    //     })
+    // }
 
     // Set the `Content-Security-Policy` header as a security measure to prevent XSS attacks
     // It works by explicitly whitelisting trusted sources of content for your website
     // This will block all inline scripts and styles except for those that are allowed
     headers.push({
-      source: '/(.*)',
       headers: [
         {
           key: 'Content-Security-Policy',
           value: ContentSecurityPolicy,
         },
       ],
+      source: '/(.*)',
     })
 
     return headers
   },
+  swcMinify: true,
 }
 
 module.exports = nextConfig
